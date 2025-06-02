10077934f166e413b1231c74bfcc4d7711cad53f
Oleksii <oleksii.bakalov@gmail.com>
2025-06-02 15:25:13 -0400

    first impl pong

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,6 +1,7 @@
 package codecrafters_redis
 
 import java.net.{InetSocketAddress, ServerSocket}
+import java.nio.charset.StandardCharsets
 
 object Server {
   def main(args: Array[String]): Unit = {
@@ -12,5 +13,27 @@ object Server {
      val serverSocket = new ServerSocket()
      serverSocket.bind(new InetSocketAddress("localhost", 6379))
      val clientSocket = serverSocket.accept() // wait for client
+
+    println("Server is running and waiting for a client to connect...")
+
+    val inputStream = clientSocket.getInputStream
+    val outputStream = clientSocket.getOutputStream
+
+    // Read the first byte from the input stream
+    val inputArray = new Array[Byte](1024) // 1 KB
+    inputStream.read(inputArray)
+    val inputString = new String(inputArray, StandardCharsets.UTF_8).trim
+    println(s"Received input: $inputString")
+    // Process the input and send a response
+    inputString match {
+      case "PING" =>
+        val response = "+PONG\r\n"
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8))
+        println("Sent response: PONG")
+      case _ =>
+        val response = "-ERR unknown command\r\n"
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8))
+        println("Sent response: ERR unknown command")
+    }
   }
 }
