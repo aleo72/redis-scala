f59e86690ac71751d4b662e6979633011048a60e
oleksii.b <oleksii.bakalov@gmail.com>
2025-06-09 14:44:06 -0400

    codecrafters submit [skip ci]

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,6 +1,6 @@
 package codecrafters_redis
 
-import java.io.OutputStream
+import java.io.{BufferedReader, Closeable, OutputStream}
 import java.net.{InetSocketAddress, ServerSocket}
 import java.nio.charset.StandardCharsets
 
@@ -11,40 +11,28 @@ object Server {
 
     // Uncomment this to pass the first stage
     //
-     val serverSocket = new ServerSocket()
-     serverSocket.bind(new InetSocketAddress("localhost", 6379))
-     val clientSocket = serverSocket.accept() // wait for client
-
+    val serverSocket = new ServerSocket()
+    serverSocket.bind(new InetSocketAddress("localhost", 6379))
+    val clientSocket = serverSocket.accept() // wait for client
+    val outputStream: OutputStream = clientSocket.getOutputStream
     log("Server is running and waiting for a client to connect...")
 
-    val inputStream = clientSocket.getInputStream
-    val outputStream: OutputStream = clientSocket.getOutputStream
+    val reader = createReader(clientSocket.getInputStream)
 
-    // Read the first byte from the input stream
-    val inputArray = new Array[Byte](1024) // 1 KB
-    inputStream.read(inputArray)
-    val inputString = new String(inputArray, StandardCharsets.UTF_8).trim
-    log(s"Received input: $inputString")
-    // Process the input and send a response
-    inputString.split("\r\n").foreach { command =>
-        log(s"Processing command: $command")
-        // Call the function to process the command
-        processCommand(command, outputStream)
+    while (true) {
+      val command = reader.readLine()
+      if (command == null)
+        shutdown(serverSocket, clientSocket)
+      log(s"Received command: $command")
+      processCommand(command, clientSocket.getOutputStream)
     }
 
-
-    // Close the streams and socket
-//    outputStream.close()
-//    inputStream.close()
-//    clientSocket.close()
-//    serverSocket.close()
-
-    println("Server has finished processing the request and is shutting down.")
+    shutdown(serverSocket, clientSocket)
   }
 
-  def processCommand(command: String, out: OutputStream): Unit = {
+  private def processCommand(command: String, out: OutputStream): Unit = {
     // This function can be extended to handle more commands
-   log(s"Processing command: $command")
+    log(s"Processing command: $command")
     command match {
       case "PING" => out.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
       case _ =>
@@ -53,8 +41,38 @@ object Server {
     }
   }
 
+  def createReader(inputStream: java.io.InputStream): BufferedReader =
+    new BufferedReader(
+      new java.io.InputStreamReader(inputStream, StandardCharsets.UTF_8)
+    )
+
   def log(message: String): Unit = {
     // This function can be used to log messages
     println(s"LOG: $message")
+  }
+
+  def shutdown(
+      serverSocket: ServerSocket,
+      clientSocket: java.net.Socket
+  ): Unit = {
+    // This function can be used to gracefully shut down the server
+    log("Shutting down the server...")
+    log("Client disconnected, shutting down server...")
+    closeStreams(serverSocket, "ServerSocket")
+    closeStreams(clientSocket, "ClientSocket")
+    sys.exit(0)
+
+  }
+
+  def closeStreams(s: Closeable, name: String): Unit = {
+    try {
+      if (s != null) {
+        s.close()
+        log(s"$name closed successfully.")
+      }
+    } catch {
+      case e: Exception =>
+        log(s"Error closing $name: ${e.getMessage}")
+    }
   }
 }
